944e4db63b7ce7897fb760d5f39d7d62d2774a94
willon <1061258787@qq.com>
2019-03-17 20:55:55 +0800

    init other service

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import path from 'path'
 import NDBC from './middleware/ndbc.js'
 import Service from './middleware/service.js'
 import Schema from './model/schema.js'
-let app = new Koa(),router = new Router(),{ user } = Schema;
+let app = new Koa(),router = new Router(),{ user,msg,company,area,sensor } = Schema;
 app.use(koaBody({
     multipart:true, // 支持文件上传
     encoding:'gzip',
@@ -22,11 +22,23 @@ app.use(koaBody({
 (async ()=>{
     try {
         const ndbc = await NDBC();
-        const UserService = Service(ndbc,'user',user);
+        const UserService = Service(ndbc,'user',user),
+            MsgService = Service(ndbc,'msg',msg),
+            CompanyService = Service(ndbc,'company',company),
+            AreaService = Service(ndbc,'area',area),
+            SensorService = Service(ndbc,'sensor',sensor);
             app.context.ndbc = ndbc;
             app.context.service = {};
             app.context.service.user = UserService;
+            app.context.service.msg = MsgService;
+            app.context.service.company = CompanyService;
+            app.context.service.area = AreaService;
+            app.context.service.sensor = SensorService;
             router.get('/user',UserService.select).post('/user',UserService.created)
+            router.get('/msg',MsgService.select).post('/msg',MsgService.created)
+            router.get('/company',CompanyService.select).post('/company',CompanyService.created)
+            router.get('/area',AreaService.select).post('/area',AreaService.created)
+            router.get('/sensor',SensorService.select).post('/sensor',SensorService.created)
             app.use(router.middleware())
             app.listen(3000,()=>{
                 console.log('server running......')
